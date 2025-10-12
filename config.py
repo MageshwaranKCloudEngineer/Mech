49a1ff23bacc8e325f343768433c72cd8cf6ad67
Mageshwaran <mageshwaran960@gmail.com>
2025-10-12 11:53:21 +0100

    Revert "Merge branch 'main' of github.com:MageshwaranKCloudEngineer/Mech"
    This reverts commit b9c2488372319a44d91d9d13c0dd36782f093d99, reversing
    changes made to e5dd4a12ff6ce610db993081fd367f2892890f7e.

---
+++ config.py
@@ -0,0 +1,32 @@
+import boto3
+import os
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+AWS_REGION = os.getenv("AWS_REGION", "us-east-1")  
+
+dynamodb = boto3.resource(
+    "dynamodb",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    aws_session_token=AWS_SESSION_TOKEN,
+    region_name=AWS_REGION
+)
+service_centers_table = dynamodb.Table("ServiceCentersTable")
+warranty_table = dynamodb.Table("WarrantyTable")
+appointment_table = dynamodb.Table("AppointmentTable")
+
+
+# Set up SNS client
+sns_client = boto3.client(
+    "sns",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    aws_session_token=AWS_SESSION_TOKEN,
+    region_name=AWS_REGION
+)
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
