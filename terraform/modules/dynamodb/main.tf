cad02835a0dc82acf077230fdc17df491fc2c7d7
Mageshwaran <mageshwaran960@gmail.com>
2025-10-19 18:13:00 +0100

    lastest

---
+++ terraform/modules/dynamodb/main.tf
@@ -0,0 +1,45 @@
+variable "env" {
+  description = "Environment name (dev/staging/prod)"
+  type        = string
+}
+
+resource "aws_dynamodb_table" "service_centers" {
+  name         = "${var.env}_ServiceCentersTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "warranty" {
+  name         = "${var.env}_WarrantyTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "appointment" {
+  name         = "${var.env}_AppointmentTable"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+output "tables" {
+  value = [
+    aws_dynamodb_table.service_centers.name,
+    aws_dynamodb_table.warranty.name,
+    aws_dynamodb_table.appointment.name
+  ]
+}
