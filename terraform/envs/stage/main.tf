d35e49e3c7e6328b8adeba05c8c00d70a57e7299
MageshwaranK <117741759+MageshwaranKCloudEngineer@users.noreply.github.com>
2025-10-21 19:23:46 +0100

    Add skip_requesting_account_id and skip_metadata_api_check

--- terraform/envs/stage/main.tf
+++ terraform/envs/stage/main.tf
@@ -4,6 +4,8 @@ provider "aws" {
   access_key = "dummy"
   secret_key = "dummy"
   skip_credentials_validation = true
+  skip_requesting_account_id  = true     # <--- ADD THIS
+  skip_metadata_api_check     = true 
   endpoints {
     dynamodb = "http://localhost:8000"
   }
